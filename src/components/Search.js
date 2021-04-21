f3b454f895377286405c2146b0c0d27fc588b13c
p-koskey <patiencekoskey@gmail.com>
2021-04-21 16:56:03 +0300

    add search button and background image

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div>
+        <form action="" className="search-form">
+       <input type="search" className="search-bar"/>
+       <button className="search-button">Search</button>
+     </form>
+        </div>
+    )
+}
+
+export default Search
