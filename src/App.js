4d466c4f09024c3fa669ef418e6bc645604c8436
p-koskey <patiencekoskey@gmail.com>
2021-04-23 16:17:22 +0300

    css changes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
   const APP_KEY = 'eeda9b6eb28c9419376afa18c0b27c82	';
   
  const [recipes, setRecipes] = useState([])
- const [searchTerm, setSearchTerm] = useState("")
+ const [search, setSearch] = useState('')
+ const [searchTerm, setSearchTerm] = useState('all')
   useEffect(() => {
-  // Fetch Recipe
+  // Fetch Recipe <p style={{color:''}}></p>
 
   const fetchRecipe = async () => {
    
@@ -20,6 +21,7 @@ function App() {
 
 
       setRecipes (data.hits)
+      console.log(data.hits)
     }
 
     fetchRecipe()
@@ -27,25 +29,27 @@ function App() {
   }, [searchTerm])
 
   
-     const handleSearchInputChanges = (e) => {
-    setSearchTerm(e.target.value);
+const handleSearchInputChanges = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value)
   }
 
   const callSearch = (e) => {
     e.preventDefault();
     
-    setSearchTerm(searchTerm);
-  
+    setSearchTerm(search);
+    setSearch('');
   }
   return (
     <div className="App">
       <nav>
         <h1>RECIPE SEARCH</h1>
+        <h4>Finding the perfect recipe for a dish can be an arduous and time-consuming task, especially if you want to do a search of multiple recipe</h4>
         </nav>
-        <form  className="search-form">
-       <input type="text" className="search-bar" value={searchTerm}
+        <form  className="search-form" onSubmit={callSearch}>
+       <input type="text" className="search-bar" value={search}
           onChange={handleSearchInputChanges}/>
-       <button className="search-button"  onClick={callSearch} type="submit" value="SEARCH">Search</button>
+       <button className="search-button"  type="submit" value="SEARCH">Search</button>
      </form>
       
     <div className="results">
@@ -53,7 +57,10 @@ function App() {
         // console.log(recipe.recipe)
         <Results key = {index} title= {recipe.recipe.label}
         image={recipe.recipe.image}
-        url = {recipe.recipe.url}/>
+        url = {recipe.recipe.url}
+        ingredients = {recipe.recipe.ingredients}
+        
+        />
     ))}
     </div>
     </div>
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const Results = ({title,image,url}) => {
+const Results = ({title,image,url,ingredients}) => {
     return (
-        <div className="result">
-           <img src={image} alt=""/>
-          <a href={url}> <p style={{color:''}}>{title}</p></a>
-            
+        <div className="result card">
+            <div className="image">
+           <img src={image} alt={title}/>
+           </div>
+           <div className="container">
+                <h2>{title}</h2> <br/>
+                <h3><b>Ingredients:</b></h3>
+                <ul>
+                {ingredients.map((ingredient, index) => 
+                    <li>{index}{ingredient.text}</li>
+                )}
+                </ul><br/>
+                
+            </div>
+         <div className="btn">
+            <button><a href={url}>See Full Recipe</a> </button>
+            </div>
         </div>
     )
 }
